1846d4dd7865012d3d7337694b2fa645f0411c74
Eva Garcia <emgg2222@gmail.com>
2023-12-20 10:36:48 +0100

    Control Props alternativo

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
 import { Product, onChangeArgs } from '../interfaces/interfaces';
 
 interface useProductArgs {
@@ -11,19 +11,7 @@ export const useProduct = ( { onChange, product, value = 0 }: useProductArgs  ) 
 
     const [counter, setCounter] = useState(value);
 
-    // se usa un useRef pq de esta manera no hay que redibujar los componentes en caso de que cambie 
-    // para saber si está controlado lo que hay que hacer es saber si tengo algun valor en el onChange
-    // pero si lo dejamos useRef(onChange) va a tener undefined o toda la función, pero no queremos la función 
-    // queremos un true o un  false, podemos transformarlo mediante !!onChange , si lo dejamos !onChange significaria si no existe el onchange
-    // con !! estariamos haciendo la negación del false entonces sería true     
-    // ahora isControlled va a tener un valor boolean 
-    const isControlled = useRef( !!onChange )
-
     const increaseBy =  ( value: number ) => {
-      if( isControlled.current ) {
-        // cuando le ponemos ! en la función es para decirle a typescript que va a existir onChange 
-        return onChange!({count: value, product })
-      }
         const newValue = Math.max( counter + value, 0)
         setCounter( newValue);
         onChange && onChange({count: newValue, product });
--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -7,26 +7,10 @@ export const useShoppingCart = () => {
     const [ shoppingCart, setShoppingCart ] = useState<{ [key:string]: ProductInCart }>({})
 
     const onProductCountChange = ({ count, product }: { count:number, product: Product}) => {
-       
-     //   console.log({ count })
       
             setShoppingCart( oldShoppingCart => {
-                const productInCart: ProductInCart = oldShoppingCart[product.id] || {...product, count:0 }
-
-                if( Math.max (productInCart.count + count , 0) > 0 ) {
-                    productInCart.count += count
-                    return {
-                        ...oldShoppingCart, 
-                        [product.id]: productInCart
-                    }
-                }
-
-                //borrar el producto
-
-                const {[product.id]: toDelete, ...rest } = oldShoppingCart
-                    return rest  
-
-       /*         if( count === 0 ) {
+       
+                if( count === 0 ) {
                     const {[product.id]: toDelete, ...rest } = oldShoppingCart
                     return rest  
                 } else {
@@ -34,7 +18,7 @@ export const useShoppingCart = () => {
                         ...oldShoppingCart, 
                         [ product.id ]: { ...product, count}
                     }
-                }*/
+                }
 
             })
 
